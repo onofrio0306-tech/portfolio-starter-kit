77517c10b6f41136a583c044191b4353142050f7
onofrio0306-tech <onofrio0306@gmail.com>
2026-05-03 21:35:58 -0500

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,8 +8,10 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
-      <BlogPosts />
+    <p className="text-sm text-neutral-400">
+  No projects yet — currently building hands-on experience.
+</p>
+      
     </section>
   )
 }
