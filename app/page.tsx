0458dbafaa213899a6c76ccdbcf629c2912ef89d
onofrio0306-tech <onofrio0306@gmail.com>
2026-05-03 21:43:46 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,16 @@ export default function Page() {
       <p className="mb-4">
         {`I’m a cybersecurity and IT networking student focused on building real-world skills in networking, systems, and security. I’ve worked on hands-on labs involving Windows Server, AWS, vulnerability scanning, and network configuration. I’m actively preparing for certifications like CompTIA A+ and exploring different areas within cybersecurity as I continue to grow my skills.`}
       </p>
-      <div className="my-8">
-  <p className="text-sm text-neutral-400">
-    Projects coming soon as I continue building hands-on experience.
-  </p>
+      <div className="mt-6 flex gap-4 text-sm">
+  <a href="https://github.com/onofrio0306-tech" target="_blank">
+    GitHub
+  </a>
+  <a href="mailto:onofrio0306@gmail.com">
+    Email
+  </a>
+  <a href="https://www.linkedin.com/in/michael-volpe1/" target="_blank">
+    LinkedIn
+  </a>
 </div>
     </section>
   )
