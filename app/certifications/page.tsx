cfe3a8d73770b0c2bcb5c9a3f704382e094d6ddf
onofrio0306-tech <onofrio0306@gmail.com>
2026-05-04 20:49:58 -0500

    Update page.tsx

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -13,22 +13,22 @@ export default function Certifications() {
       >
         <div>
           <h3>CAE2Y Cybersecurity – WCTC (2026)</h3>
-          <img src="/certs/cae2y.jpg" alt="CAE2Y Cybersecurity" style={{ width: "100%" }} />
+          <img src="/certs/cae2y.png" alt="CAE2Y Cybersecurity" style={{ width: "100%" }} />
         </div>
 
         <div>
           <h3>IT Security Administrator – WCTC (2025)</h3>
-          <img src="/certs/admin.jpg" alt="IT Security Administrator" style={{ width: "100%" }} />
+          <img src="/certs/admin.png" alt="IT Security Administrator" style={{ width: "100%" }} />
         </div>
 
         <div>
           <h3>IT Security Manager – WCTC (2025)</h3>
-          <img src="/certs/manager.jpg" alt="IT Security Manager" style={{ width: "100%" }} />
+          <img src="/certs/manager.png" alt="IT Security Manager" style={{ width: "100%" }} />
         </div>
 
         <div>
           <h3>Google Cybersecurity Certificate (2025)</h3>
-          <img src="/certs/google.jpg" alt="Google Cybersecurity Certificate" style={{ width: "100%" }} />
+          <img src="/certs/google.png" alt="Google Cybersecurity Certificate" style={{ width: "100%" }} />
         </div>
       </div>
     </div>
