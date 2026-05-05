704755a46707be35ffad50e9dbd9b05878bed796
onofrio0306-tech <onofrio0306@gmail.com>
2026-05-04 20:56:41 -0500

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,31 +1,25 @@
 import Link from 'next/link'
 
 const navItems = {
-  '/': {
-    name: 'Home',
-  },
+  '/': { name: 'Home' },
+  '/certifications': { name: 'Certifications' },
 }
 
 export function Navbar() {
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              )
-            })}
+        <nav className="flex flex-row items-start px-0 pb-0">
+          <div className="flex flex-row space-x-2 pr-10">
+            {Object.entries(navItems).map(([path, { name }]) => (
+              <Link
+                key={path}
+                href={path}
+                className="px-3 py-1 rounded-md transition-all hover:bg-neutral-800 hover:text-white"
+              >
+                {name}
+              </Link>
+            ))}
           </div>
         </nav>
       </div>
